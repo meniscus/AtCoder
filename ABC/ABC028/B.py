b0f216f5e7084c05a36f55a8bc9b435523e8da56
meniscus <kyoro5454@hotmail.com>
2018-11-15 13:49:51 +0900

    solve ABC028 B

---
+++ ABC/ABC028/B.py
@@ -0,0 +1,3 @@
+l = list(input())
+out_str = str(l.count("A")) + " " + str(l.count("B")) + " " + str(l.count("C")) + " " + str(l.count("D")) + " " + str(l.count("E")) + " " + str(l.count("F"))
+print(out_str)

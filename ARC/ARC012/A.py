d09768efe36f13ab69ebea041916955722c32cd7
Kyosuke Yoshida <Kyosuke Yoshida>
2019-01-29 00:27:24 +0900

    solve ARC012 A

---
+++ ARC/ARC012/A.py
@@ -0,0 +1,15 @@
+day = input()
+
+if (day == "Sunday" or day == "Saturday") :
+    print(0)
+elif (day == "Monday") :
+    print(5)
+elif (day == "Tuesday") :
+    print(4)
+elif (day == "Wednesday") :
+    print(3)
+elif (day == "Thursday") :
+    print(2)
+elif (day == "Friday") :
+    print(1)
+
